038eb358e51618db0ff88cb97c7aba0d4dbb4676
chriistopherngo <ngo.chris@hotmail.com>
2024-04-28 20:19:35 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [msg, setMsg] = useState();
+
+  const newMsg = () => {
+    setMsg("dette kosta meg 95kr");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ChakraProvider>
+        <h1>halla</h1>
+        <Button colorScheme="red" onClick={() => newMsg()}>
+          Button
+        </Button>
+
+        {msg && 
+          <p>{msg}</p>
+        }
+      </ChakraProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

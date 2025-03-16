eb256b57f74b7da013bf191f1fdddd03d3ae6926
chriistopherngo <ngo.chris@hotmail.com>
2025-03-16 03:34:06 +0100

    fixes

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,25 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Intro.css";
 
 const Intro = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const checkMobile = () => {
+    if (window.innerWidth <= 600) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  useEffect(() => {
+    checkMobile();
+
+    window.addEventListener("resize", checkMobile);
+
+    return () => {
+      window.removeEventListener("resize", checkMobile);
+    };
+  }, []); 
+
   return (
     <div className="wrapper">
-      <div className="intro_container">
-        <div className="intro_text">
-          <h1>
-            Hi, I'm <span class="gradient_name">Christopher Ngo</span>.
-          </h1>
-          {/* <p>Web Development Student at NTNU Gjøvik.</p> */}
-          <p> I'm a <span className="gradient_name">Full-stack Web Developer</span> based in Gjøvik, Norway. I turn ideas into real applications.</p>
-        </div>
-        {/* <div className="intro_image">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
-            // src="/img/me3.png"
-            alt="Me"
-          />
-        </div> */}
-      </div>
+      {!isMobile && (
+        <>
+          <div className="intro_container">
+            <div className="intro_text">
+              <h1>
+                Hi, I'm <span className="gradient_name">Christopher Ngo</span>.
+              </h1>
+              <p>
+                I'm a <span className="gradient_name">Full-stack Web Developer</span> based in Gjøvik, Norway. I turn ideas into real applications.
+              </p>
+            </div>
+            <div className="intro_image_outer">
+              <div className="intro_image_inner">
+                <img src="/img/me.jpg" alt="Me" className="image_me" />
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+
+      {isMobile && (
+        <>
+          <div className="intro_container">
+            <div className="intro_text">
+              <h1>
+                Hi, I'm <span className="gradient_name">Christopher Ngo</span>.
+              </h1>
+              <div className="intro_image_outer mobile">
+                <div className="intro_image_inner">
+                  <img src="/img/me.jpg" alt="Me" className="image_me " />
+                </div>
+              </div>
+            </div>
+            <p className="intro_text_mobile">
+              I'm a <span className="gradient_name">Full-stack Web Developer</span> based in Gjøvik, Norway. I turn ideas into real applications.
+            </p>
+          </div>
+        </>
+      )}
     </div>
   );
 };
